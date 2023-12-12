089e9ab80934db6f21d8c0d13827cc547a451147
zman-1x1 <saicharankandukuri1x1@gmail.com>
2023-12-12 15:04:42 +0530

    chore: change variable name according to bknd api | + formatting

--- src/app/components/teacher-profile/teacher-profile.component.ts
+++ src/app/components/teacher-profile/teacher-profile.component.ts
@@ -3,49 +3,56 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-teacher-profile',
   templateUrl: './teacher-profile.component.html',
-  styleUrls: ['./teacher-profile.component.scss']
+  styleUrls: ['./teacher-profile.component.scss'],
 })
 export class TeacherProfileComponent {
-[x: string]: any;
+  [x: string]: any;
   teacherName: string = '';
   teacherLivingCity: string = '';
   teacherContactNumber: string = '';
   teacherEmail: string = '';
   role: string = '';
-  intrestedSchool: string = '';
-  intrestedSchoolLocation: string = '';
-  question: string = '';
+  school_private_insititute_interested: string = '';
+  school_private_insititute_name: string = '';
+  intrestedSchoolCity: string = ''; // city
+  specific_question: string = '';
 
   highlightedButtons: { [key: string]: boolean } = {};
   // ["school_teacher","admin","principal","owner","private_tutor","staff","hod","other",]
   roleButton: string[][] = [
     ['school_teacher', 'admin'],
-    ["principal", "owner"],
-    ["private_tutor", "staff"],
-    ["hod", "other"],
+    ['principal', 'owner'],
+    ['private_tutor', 'staff'],
+    ['hod', 'other'],
   ];
 
   roleButtonHumanReadable: string[][] = [
     ['School Teacher', 'Admin'],
-    ["Principal", "Owner"],
-    ["Private Tutor", "Staff"],
-    ["HOD", "Other"],
+    ['Principal', 'Owner'],
+    ['Private Tutor', 'Staff'],
+    ['HOD', 'Other'],
   ];
 
-  role1DArray: string[] = this.roleButton.reduce((acc, val) => acc.concat(val), []);
-  roleHumanReadable1DArray: string[] = this.roleButtonHumanReadable.reduce((acc, val) => acc.concat(val), []);
+  role1DArray: string[] = this.roleButton.reduce(
+    (acc, val) => acc.concat(val),
+    []
+  );
+  roleHumanReadable1DArray: string[] = this.roleButtonHumanReadable.reduce(
+    (acc, val) => acc.concat(val),
+    []
+  );
 
   isHighlighted(buttonId: string) {
     return this.highlightedButtons[buttonId];
   }
   handleRoleButton(value: string) {
-    console.log(value);
+    // console.log(value);
     this.role = value;
   }
 
   toggleHighlight(buttonId: string) {
     this.highlightedButtons[buttonId] = !this.highlightedButtons[buttonId];
-    
+
     // remove highlight from other buttons
     this.roleButtonHumanReadable.forEach((row) => {
       row.forEach((id) => {
@@ -56,6 +63,13 @@ export class TeacherProfileComponent {
     });
 
     // set role
-    this.handleRoleButton(this.role1DArray[this.roleHumanReadable1DArray.indexOf(buttonId)])
+    this.handleRoleButton(
+      this.role1DArray[this.roleHumanReadable1DArray.indexOf(buttonId)]
+    );
+  }
+
+  handleSchoolTypeButton(value: string) {
+    console.log(value);
+    this.school_private_insititute_interested = value;
   }
 }
