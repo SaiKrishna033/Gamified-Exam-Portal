e8b6e0d7f126fa2e0a33102674dbdbf941bd8128
zman-1x1 <saicharankandukuri1x1@gmail.com>
2023-12-14 15:08:28 +0530

    feat: Add encryption service to user service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from "@angular/core";
+import { EncryptionService } from './shared/services/encryption.service';
+import { Inject } from "@angular/core";
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  constructor(
+    @Inject(EncryptionService) private keySvc: EncryptionService
+  ) {}
   getUserType(): string | null {
     // Logic to fetch user type from local storage or API
     // Return null if user type is not available or not authenticated
-    return localStorage.getItem('userType');
+    return this.keySvc.decryptData(localStorage.getItem('userType') as string);
   }
   getAccessToken(): string | null {
     // Logic to fetch access token from local storage or API
     // Return null if access token is not available or not authenticated
-    return localStorage.getItem('accessToken');
+    return this.keySvc.decryptData(localStorage.getItem('accessToken') as string);
   }
 }
